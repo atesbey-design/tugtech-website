f14b34283dedcb7de13004d1c2dbf3bf61fa066f
ebrartamer~ <ebrartamer21@gmail.com>
2024-01-30 19:33:30 +0300

    Fix:build err

--- data/clients.ts
+++ data/clients.ts
@@ -1,69 +1,61 @@
-import { getCookie } from 'cookies-next'
-
-export const INDEV = process.env.NODE_ENV === 'development'
-const apiurl =  'http://localhost:32500/'
-
+export const INDEV = process.env.NODE_ENV === "development";
+const apiurl = "http://localhost:32500/";
 
 export const api: any = {
   get: async (endpoint: string, data: any) => {
     const request = await fetch(apiurl + endpoint, {
-      method: 'GET',
-     
-     
-    })
+      method: "GET",
+    });
 
     if (request.status === 200) {
-      return request.json()
+      return request.json();
     }
 
-    throw request
+    throw request;
   },
   post: async (endpoint: string, data: any) => {
     const request = await fetch(apiurl + endpoint, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json',
-    
+        "Content-Type": "application/json",
       } as any,
-    })
+    });
 
     if ([200, 201].indexOf(request.status) !== -1) {
-      return request.json()
+      return request.json();
     }
 
-    throw request
+    throw request;
   },
   postFile: async (endpoint: string, data: any) => {
     const request = await fetch(apiurl + endpoint, {
-      method: 'POST',
+      method: "POST",
       body: data,
-   
-    })
+    });
     if ([200, 201].indexOf(request.status) !== -1) {
       try {
-        return await request.json()
+        return await request.json();
       } catch (e) {
-        return request
+        return request;
       }
     }
   },
   buffer: async (endpoint: string) => {
     const request = await fetch(apiurl + endpoint, {
-      method: 'GET',
-
-    })
+      method: "GET",
+    });
 
     if (request.status === 200) {
-      return request.arrayBuffer()
+      return request.arrayBuffer();
     }
 
-    throw new Error('Failed to fetch buffer')
+    throw new Error("Failed to fetch buffer");
   },
-}
+};
 
 export const clients = {
   api,
-}
+};
 
-export default clients
+export default clients;
